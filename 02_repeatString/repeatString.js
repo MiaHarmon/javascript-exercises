8d4bb15ea2bb283c51ba48f1f3c5c538efa0fcea
Mia Harmon <mia.harmon@yahoo.com>
2024-02-01 22:45:02 -0500

    Add fundamentals 4 solutions

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,17 @@
-const repeatString = function() {
+const repeatString = function(givenString, repeatNumber) {
+let result = "";
 
+if (repeatNumber < 0){
+    return 'ERROR';
+}
+if (givenString == ""){
+    return "";
+}
+for (let i = 0; i<repeatNumber; i++){
+    result += givenString;
+}
+
+return result;
 };
 
 // Do not edit below this line
